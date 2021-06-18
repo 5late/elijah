682e5066d0795540a231c042121813fc0d532f72
5late <christianrfernandes5@gmail.com>
2021-06-18 18:03:57 -0400

    updated maingo to check for changes

--- pull/main.go
+++ pull/main.go
@@ -2,12 +2,57 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
+	"os"
 
 	"github.com/go-git/go-git/v5"
 )
 
+const chunkSize = 64000
+
 var path = "/home/all/repos/elijah/"
+var filename = "../push/example-file.txt"
+var comparer = "../push/example-file2.txt"
+
+func mainCheck() {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	secondData, err := ioutil.ReadFile(comparer)
+	if err != nil {
+		log.Fatal(err)
+	}
+	print(string(secondData))
+	print(string(data))
+
+	if string(secondData) == string(data) {
+		print("same")
+	}
+}
+
+func Copy(src, target string) error {
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	out, err := os.Create(target)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, in)
+	if err != nil {
+		return err
+	}
+	return out.Close()
+}
 
 func checkForChanges() bool {
 	r, err := git.PlainOpen(path)
@@ -23,6 +68,7 @@ func checkForChanges() bool {
 
 func main() {
 	if checkForChanges() {
+		mainCheck()
 		return
 	}
 	// We instantiate a new repository targeting the given path (the .git folder)
@@ -53,4 +99,5 @@ func main() {
 	}
 
 	fmt.Println(commit)
+	Copy(filename, comparer)
 }
