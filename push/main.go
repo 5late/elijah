07aa9491169520743fed3e84b605fc91300b7cbc
5late <christianrfernandes5@gmail.com>
2021-06-18 22:12:18 -0400

    merge and push golang

--- push/main.go
+++ push/main.go
@@ -16,8 +16,8 @@ import (
 
 const chunkSize = 64000
 
-var filename = "example-file.txt"
-var comparer = "example-file2.txt"
+var filename = "push/example-file.txt"
+var comparer = "push/example-file2.txt"
 
 func Copy(src, target string) error {
 	in, err := os.Open(src)
